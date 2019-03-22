1575dfb9476a9dd65bdecbdb59567fd2e168fc7b
David Cantu <david.cantu.delgado@tec.mx>
2019-03-21 20:56:02 -0600

    Initial Commit

---
+++ Todo-Firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo-Firebase
+//
+//  Created by David A Cantú Delgado on 3/21/19.
+//  Copyright © 2019 Tec de Monterrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
